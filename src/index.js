b0e40dfa84e245deb9f8ef1df560017a1770ac66
MrPatesz <patesz1999@gmail.com>
2020-09-27 19:30:12 +0200

    Godly Fixes

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import ReactDOM, { unstable_renderSubtreeIntoContainer } from 'react-dom';
+import ReactDOM/*, { unstable_renderSubtreeIntoContainer }*/ from 'react-dom';
 import './index.css';
 import * as serviceWorker from './serviceWorker';
 import Song from './Song.js';
